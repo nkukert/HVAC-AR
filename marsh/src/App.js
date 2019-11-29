9c197f65131b38973407ab50d43470a12364c480
nkukert <nikolas.kukert@ndsu.edu>
2019-11-29 13:13:57 -0600

    added corner cards and started initial positioning

--- marsh/src/App.js
+++ marsh/src/App.js
@@ -19,20 +19,50 @@ function App() {
         direction='row'
         justify='flex-start'
         alignItems='flex-start'>
-          <Grid item xs={12}>
-            <Card className='welcomeCard'>Oh hai</Card>
+          <Grid item xs={3}>
+            <div className = 'topLeft'>
+                <Card className='welcomeCard'>Oh hai</Card>
+            </div>
+          </Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={3}>
+          <div className = 'topRight'>
+                <Card className='welcomeCard'>Oh hai</Card>
+              </div>
           </Grid>
 
         </Grid>
-        <Card className="welcomeCard">
-          <CardContent>
-          <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-          </CardContent>
-        </Card>
+        <Grid>
+          <div className='welcomeDiv'>
+          <Card className="welcomeCard">
+            <CardContent>
+            <Typography variant="body2" component="p">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+            </CardContent>
+          </Card>
+          </div>
+        </Grid>
+
+        <Grid
+        container
+        direction='row'
+        justify='flex-start'
+        alignItems='flex-start'>
+        <Grid item xs={3}>
+            <div className = 'bottomLeft'>
+                <Card className='welcomeCard'>Oh hai</Card>
+            </div>
+          </Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={3}>
+          <div className = 'bottomRight'>
+                <Card className='welcomeCard'>Oh hai</Card>
+              </div>
+          </Grid>
+        </Grid>
       </header>
       
     </div>
