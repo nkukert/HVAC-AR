fd39ba91a4970831344df36c1ccc6940f3957387
Sev <41923741+nkukert@users.noreply.github.com>
2019-11-29 15:35:01 -0600

    Ui2 (#1)
    * experimenting with material ui
    
    * removed material ui dependency and brought back bootstrap button
    
    * added card and carddeck elements and began dom selection for css
    
    * added functionality for second ui draft
    
    * added corner cards and started initial positioning
    
    * finished second draft of ui

--- marsh/src/App.js
+++ marsh/src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import logo from './logo.svg';
+import Card from '@material-ui/core/Card';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
+        {/*<p>
           Welcome to the development build of MARSH!
-        </p>
-        <Button
-          variant="info"
-          href="https://reactjs.org"
-          size="lg"
-        >
-          Get Started
-        </Button>
+        </p>*/}
+        <Grid
+        container
+        spacing={3}
+        direction='row'
+        justify='flex-start'
+        alignItems='flex-start'>
+          <Grid item xs>
+            <div>
+                <Card className='topLeft'>Explore</Card>
+            </div>
+          </Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs>
+            <div>
+              <Card className='topRight'>Test</Card>
+            </div>
+          </Grid>
+
+        </Grid>
+        <Grid>
+          <div className='welcomeDiv'>
+          <Card className="welcomeCard">
+            <CardContent>
+            <Typography variant="body2" component="p">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+            </CardContent>
+          </Card>
+          </div>
+        </Grid>
+
+        <Grid
+        container
+        direction='row'
+        justify='flex-start'
+        alignItems='flex-start'>
+        <Grid item xs={3}>
+            <div>
+                <Card className='bottomLeft'>Evaluate</Card>
+            </div>
+          </Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={3}>
+          <div>
+                <Card className='bottomRight'>Create</Card>
+              </div>
+          </Grid>
+        </Grid>
       </header>
+      
     </div>
+    
   );
 }
 
